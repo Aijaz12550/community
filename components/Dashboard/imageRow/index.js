238277841924e0346e1df9e481cb597d33e74ff2
Muhammad Owais Warsi <owaiskhan148@gmail.com>
2020-04-15 01:21:44 +0500

    Member details modal inline styling convert in scss

--- components/Dashboard/imageRow/index.js
+++ components/Dashboard/imageRow/index.js
@@ -1,143 +1,166 @@
-import React, { Component } from 'react';
-import { Image, Badge } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Image, Badge } from "react-bootstrap";
 
 class _Images extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            width: 0,
-            height: 0,
-            showData: 4
-        };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+      showData: 4,
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState(
+      { width: window.innerWidth, height: window.innerHeight },
+      () => this.componentFunctionality()
+    );
+  }
+
+  componentFunctionality = () => {
+    if (this.state.width >= 2000) {
+      this.setState({
+        showData: 6,
+      });
+    } else if (this.state.width >= 1800) {
+      this.setState({
+        showData: 5,
+      });
+    } else if (this.state.width >= 1600) {
+      this.setState({
+        showData: 4,
+      });
+    } else if (this.state.width >= 1400) {
+      this.setState({
+        showData: 3,
+      });
+    } else if (this.state.width >= 1200) {
+      this.setState({
+        showData: 2,
+      });
+    } else if (this.state.width >= 1000) {
+      this.setState({
+        showData: 1,
+      });
+    } else {
+      this.setState({
+        showData: 0,
+      });
     }
+  };
 
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
+  render() {
+    return (
+      <div
+        key={this.props.Uniquekey + +Date.now() + 5765}
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        {this.props.familyMember.map((v, i) => {
+          let margin = 0;
+          let zIndex = 0;
+          if (i < this.state.showData) {
+            if (i == 0) {
+              margin = 0;
+              zIndex = 7;
+            } else if (i == 6) {
+              margin = -10;
+              zIndex = 6;
+            } else if (i == 5) {
+              margin = -10;
+              zIndex = 5;
+            } else if (i == 4) {
+              margin = -10;
+              zIndex = 4;
+            } else if (i == 3) {
+              margin = -10;
+              zIndex = 3;
+            } else if (i == 2) {
+              margin = -10;
+              zIndex = 2;
+            } else if (i == 1) {
+              margin = -10;
+              zIndex = 1;
+            }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
+            return (
+              <Image
+                key={i + Math.floor(Math.random() * 10001)}
+                style={{ marginLeft: margin, zIndex: zIndex }}
+                className="roundedCircle"
+                src={v.memberImage}
+              />
+            );
+          }
+        })}
+        {this.props.familyMember.length > this.state.showData ? (
+          <Badge
+            style={{
+              width: 47.5,
+              height: 47.5,
+              borderRadius: 24,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontFamily: "SF Pro Display",
+              fontStyle: "normal",
+              fontWeight: "bold",
+              fontSize: 16,
+              color: "#344563",
+              background: "#dadee2",
+              marginLeft: -10,
+            }}
+          >
+            +
+            {this.props.familyMember.length > this.state.showData
+              ? this.props.familyMember.length - this.state.showData
+              : 0}
+          </Badge>
+        ) : (
+          <></>
+        )}
+        <div
+          style={{
+            marginLeft: 5,
+            fontFamily: "Open Sans",
+            fontStyle: "normal",
+            fontWeight: 600,
+            fontSize: 14,
+            color: "#344563",
+          }}
+        >
+          {this.props.familyMember.map((v, i, arr) => {
+            if (i < this.state.showData) {
+              return (
+                <span key={i + Math.floor(Math.random() * 10001)}>
+                  {v.memberName},
+                </span>
+              );
+            }
+          })}
 
-    updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight }, () => this.componentFunctionality());
-    }
-
-    componentFunctionality = () => {
-        if (this.state.width >= 2000) {
-            this.setState({
-                showData: 6
-            })
-        } else if (this.state.width >= 1800) {
-            this.setState({
-                showData: 5
-            })
-        } else if (this.state.width >= 1600) {
-            this.setState({
-                showData: 4
-            })
-        } else if (this.state.width >= 1400) {
-            this.setState({
-                showData: 3
-            })
-        } else if (this.state.width >= 1200) {
-
-            this.setState({
-                showData: 2
-            })
-        } else if (this.state.width >= 1000) {
-
-            this.setState({
-                showData: 1
-            })
-        } else {
-            this.setState({
-                showData: 0
-            })
-        }
-    }
-
-
-    render() {
-        return (
-            <div key={this.props.Uniquekey+(+Date.now())+5765} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                {this.props.familyMember.map((v, i) => {
-                    let margin = 0;
-                    let zIndex = 0;
-                    if (i < this.state.showData) {
-                        if (i == 0) {
-                            margin = 0;
-                            zIndex = 7;
-                        } else if (i == 6) {
-                            margin = -10;
-                            zIndex = 6;
-                        } else if (i == 5) {
-                            margin = -10;
-                            zIndex = 5;
-                        } else if (i == 4) {
-                            margin = -10;
-                            zIndex = 4;
-                        } else if (i == 3) {
-                            margin = -10;
-                            zIndex = 3;
-                        } else if (i == 2) {
-                            margin = -10;
-                            zIndex = 2;
-                        } else if (i == 1) {
-                            margin = -10;
-                            zIndex = 1;
-                        }
-
-                        return (
-                             <Image key={i+Math.floor(Math.random() * 10001)}  style={{ marginLeft: margin, zIndex: zIndex }}  className="roundedCircle" src={v.memberImage} />
-
-                        )
-                    }
-                })
-                }
-                {
-                    this.props.familyMember.length > this.state.showData ?
-                        < Badge style={{
-                            width: 47.5,
-                            height: 47.5,
-                            borderRadius: 24,
-                            display: 'flex', alignItems: 'center', justifyContent: 'center',
-                            fontFamily: 'SF Pro Display',
-                            fontStyle: 'normal',
-                            fontWeight: 'bold',
-                            fontSize: 16,
-                            color: '#344563',
-                            background: '#dadee2',
-                            marginLeft: -10
-                        }} >
-                            +{this.props.familyMember.length > this.state.showData ? this.props.familyMember.length - this.state.showData : 0}
-                        </Badge>
-                        :
-                        <></>
-                }
-                <div style={{
-                    marginLeft: 5,
-                    fontFamily: 'Open Sans',
-                    fontStyle: 'normal',
-                    fontWeight: 600,
-                    fontSize: 14,
-                    color: '#344563'
-                }}>
-                    {this.props.familyMember.map((v, i, arr) => {
-                        if (i < this.state.showData) {
-                            return (
-
-                                <div key={i+Math.floor(Math.random() * 10001)}>{v.memberName},</div>
-                            )
-                        }
-                    })}
-
-                    {this.props.familyMember.length > this.state.showData ? ` & ${this.props.familyMember.length > this.state.showData ? this.props.familyMember.length - this.state.showData : 0} Others` : <></>}
-                </div>
-            </div >
-        )
-    }
+          {this.props.familyMember.length > this.state.showData ? (
+            ` & ${
+              this.props.familyMember.length > this.state.showData
+                ? this.props.familyMember.length - this.state.showData
+                : 0
+            } Others`
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 export default _Images;
