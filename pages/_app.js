f1a2db1cadd736723be1200dedd0fb87a194801d
Muhammad Owais Warsi <muhammadowaiswarsi@gmail.com>
2020-04-28 03:23:26 +0500

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,16 @@ import { Provider } from "react-redux";
 import App from "next/app";
 import withRedux from "next-redux-wrapper";
 import { initStore } from "../redux/configureStore";
-import { register } from "next-offline/runtime";
 import { withRouter } from "next/router";
-import SignIn from "./signIn";
-import "../styles/sidebar/index.scss"
-import "../styles/assets/css/dashboard.css"
+import Signin from "./signIn";
+import Viewresidents from "./dashboard/[user]/[role].js";
 
 export default withRouter(
   withRedux(initStore, { debug: true })(
     class MyApp extends App {
+      state = {
+        ComponentTorender: this.props.Component,
+      };
       // static async getInitialProps({ Component, ctx }) {
       //   return {
       //     pageProps: {
@@ -20,28 +21,107 @@ export default withRouter(
       //     }
       //   };
       // }
-      componentDidMount() {
-        register();
-      }
-      render() {
-        const { Component, pageProps, store, stars, ctx, router } = this.props;
 
+      componentDidUpdate(prevProps) {
+        const { store, router } = this.props;
         let user = store?.getState()?.AuthReducer?.user;
-        console.log("user", user);
+        if (
+          prevProps.Component != this.props.Component &&
+          user.access_token &&
+          !router.pathname.startsWith("/signIn")
+        ) {
+          this.setState({
+            ComponentTorender: this.props.Component,
+          });
+        } else if (
+          !user.access_token &&
+          router.pathname.startsWith("/signIn") &&
+          prevProps.Component != this.props.Component
+        ) {
+          console.log("jjdgjhg", this.props.Component);
+          this.setState({
+            ComponentTorender: Signin,
+          });
+        }
 
-        if (user.access_token)
-          return (
-            <Provider store={store}>
-              <Component store={store.getState()} stars={stars} ctx={ctx} />
-            </Provider>
+        if (
+          router.pathname.startsWith("/signIn") &&
+          !user.access_token &&
+          prevProps.Component != this.props.Component
+        ) {
+          return false;
+        } else if (
+          !router.pathname.startsWith("/signIn") &&
+          !user.access_token &&
+          prevProps.Component != this.props.Component
+        ) {
+          this.setState(
+            {
+              ComponentTorender: Signin,
+            },
+            () => {
+              router.push("/signIn");
+            }
           );
-        else {
-          return (
-            <Provider store={store}>
-              <SignIn store={store.getState()} stars={stars} ctx={ctx} />
-            </Provider>
+        } else if (
+          router.pathname.startsWith("/signIn") &&
+          user.access_token &&
+          prevProps.Component != this.props.Component
+        ) {
+          this.setState(
+            {
+              ComponentTorender: Viewresidents,
+            },
+            () => {
+              router.push(
+                "/dashboard/[user]/[role]",
+                `/dashboard/user_role/view_residents`
+              );
+            }
           );
         }
+      }
+
+      componentDidMount() {
+        const { store, router } = this.props;
+        let user = store?.getState()?.AuthReducer?.user;
+        if (router.pathname.startsWith("/signIn") && !user.access_token) {
+          return false;
+        } else if (
+          !router.pathname.startsWith("/signIn") &&
+          !user.access_token
+        ) {
+          this.setState(
+            {
+              ComponentTorender: Signin,
+            },
+            () => {
+              router.push("/signIn");
+            }
+          );
+        } else if (router.pathname.startsWith("/signIn") && user.access_token) {
+          this.setState(
+            {
+              ComponentTorender: Viewresidents,
+            },
+            () => {
+              router.push(
+                "/dashboard/[user]/[role]",
+                `/dashboard/user_role/view_residents`
+              );
+            }
+          );
+        }
+      }
+      render() {
+        const { store } = this.props;
+        let { ComponentTorender } = this.state;
+
+        return (
+          <Provider store={store}>
+            <ComponentTorender />
+          </Provider>
+        );
       }
     }
   )
