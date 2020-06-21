0472ff5b5550a6899b11b74ffce3714aa07d86d6
Muhammad Owais Warsi <muhammadowaiswarsi@gmail.com>
2020-06-21 21:10:22 +0500

    Refactor progress bar

--- components/invitationCode/progressBar.js
+++ components/invitationCode/progressBar.js
@@ -5,7 +5,7 @@ import Stepper from 'react-stepper-horizontal';
 export const Progress = ({ val, step }) => {
   return (
     <div style={{ width: "100%" }}>
-      <Stepper steps={step} activeStep={val} defaultBarColor='red' />
+      <Stepper steps={step} activeStep={val} size={20} />
       {/* <ProgressBar variant="success" color="red" now={val} /> */}
     </div>
   );
